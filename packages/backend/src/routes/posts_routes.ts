1c9852842a88464351ea697782027f8ae2d72153
Peretz Tubul <ptubul@gmail.com>
2024-08-04 23:27:55 +0300

    add change in posts routes

--- packages/backend/src/routes/posts_routes.ts
+++ packages/backend/src/routes/posts_routes.ts
@@ -3,12 +3,12 @@ import { Router } from "express";
 import authMiddleware from "../middlewares/auth_middleware";
 
 const router = Router();
+router.get("/:id", authMiddleware, posts.getPost);
+router.get("/", authMiddleware, posts.getAllPosts);
+router.get("/myPosts", authMiddleware, posts.getMyPosts);
 
 router.post("/", authMiddleware, posts.addPost);
 router.delete("/:id", authMiddleware, posts.deletePost);
 router.put("/:id", authMiddleware, posts.updatePost);
-router.get("/:id", authMiddleware, posts.getPost);
-router.get("/", authMiddleware, posts.getAllPosts);
-router.get("/myPosts", authMiddleware, posts.getMyPosts);
 
 export default router;
